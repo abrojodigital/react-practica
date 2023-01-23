a960cb84304501e3c8030a94af54c0387e6b9ea1
abrojodigital <abrojodigital@gmail.com>
2023-01-23 19:56:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-practica
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vemutx)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hola!</h1>
+      <p>Probando a ver si pasa algo :)</p>
+    </div>
+  );
+}
